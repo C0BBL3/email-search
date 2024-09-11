dd41d545e12ce6daa74f2ba0e2d651d6c3a4c066
Colby Roberts <colbiverse@gmail.com>
2024-09-11 10:10:05 -0700

    first commit

---
+++ dist/extension.js
@@ -0,0 +1,28 @@
+"use strict";
+(() => {
+  // src/extension.js
+  var loaderId = setInterval(() => {
+    if (!window._gmailjs) {
+      return;
+    }
+    clearInterval(loaderId);
+    startExtension(window._gmailjs);
+  }, 100);
+  function startExtension(gmail) {
+    console.log("Extension loading...");
+    window.gmail = gmail;
+    gmail.observe.on("load", () => {
+      const userEmail = gmail.get.user_email();
+      console.log("Hello, " + userEmail + ". This is your extension talking!");
+      gmail.observe.on("view_email", (domEmail) => {
+        console.log("Looking at email:", domEmail);
+        const emailData = gmail.new.get.email_data(domEmail);
+        console.log("Email data:", emailData);
+      });
+      gmail.observe.on("compose", (compose) => {
+        console.log("New compose window is opened!", compose);
+      });
+    });
+  }
+})();
+//# sourceMappingURL=extension.js.map
---
+++ main.js
@@ -0,0 +1,4 @@
+InboxSDK.load(2, 'sdk_EmailAssistant_58cc1ead5f').then(function(sdk){
+    // your app code using 'sdk' goes in here
+    console.log('hello world')
+  });
